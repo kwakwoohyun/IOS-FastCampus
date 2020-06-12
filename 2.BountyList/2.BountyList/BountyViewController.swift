0cfb5313f16f939baa41e96a3df3f4a47d1cb636
kwakwoohyun010@gmail.com <kwakwoohyun010@gmail.com>
2020-06-12 17:39:31 +0900

    feat: Add table view and update BountyViewController

--- 2.BountyList/2.BountyList/BountyViewController.swift
+++ 2.BountyList/2.BountyList/BountyViewController.swift
@@ -7,24 +7,35 @@
 //
 
 import UIKit
-
-class BountyViewController: UIViewController {
-
+// Table View를 사용하기위해 UITableVIewDataSource, UITableViewDelegate 상속받음
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+//    UITableViewDataSource
+//    몇개니
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 2
+        
     }
-    */
-
+//    어떻게 표현할거니
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+        
+    }
+//    클릭했을때 어떻게 반응할거야 UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("---> \(indexPath.row)")
+    }
+        
+    
 }

bd8cdc67786fc554ba7dfc2ea450d050bcea1bb7
kwakwoohyun010@gmail.com <kwakwoohyun010@gmail.com>
2020-06-13 16:54:49 +0900

    feat: Udpate img and add custom table view cell

--- 2.BountyList/2.BountyList/BountyViewController.swift
+++ 2.BountyList/2.BountyList/BountyViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 // Table View를 사용하기위해 UITableVIewDataSource, UITableViewDelegate 상속받음
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let nameList = ["brook","chopper","franky","luffy","nami","robin","sanji","zoro"]
+    let bountyList = [33000000,50,44000000,300000000,16000000,80000000,77000000,120000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +24,28 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
 //    몇개니
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 2
+        return bountyList.count
         
     }
 //    어떻게 표현할거니
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
+//        cell = tableview 이므로 우리는 ListCell을 사용하고싶어 캐스팅한다.
+//        옵셔널 타입일수 있으므로 바인딩한다.
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imageView?.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
         return cell
+        
+//        if-else 랩핑 방법
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
+//            return cell
+//        } else {
+//            return UITableViewCell
+//        }
         
     }
 //    클릭했을때 어떻게 반응할거야 UITableViewDelegate
@@ -37,5 +53,11 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
         print("---> \(indexPath.row)")
     }
         
-    
+}
+// Custom Cell
+// cell의 identity inspector에 ListCell class를 추가 
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
 }
